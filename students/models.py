70759d1e5833740d4b267c8f0a12d977225d7c86
Igor Tepliuk <itepliuk@gmail.com>
2018-04-15 23:16:55 +0300

    create model-student, added media root

--- students/models.py
+++ students/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    """Student model"""
+    
+    first_name = models.CharField(
+        "Ім'я",
+        max_length=256,
+        blank=False
+        )
+
+    last_name = models.CharField(
+        "Прізвище",
+        max_length=256,
+        blank=False
+        )
+
+    middle_name = models.CharField(
+        "По-батькові",
+        max_length=256,
+        blank=True,
+        default=''
+        )
+
+    birthday = models.DateField(
+        "Дата народження",
+        blank=False,
+        null=True
+        )
+
+    photo = models.ImageField(
+        "Фото",
+        blank=True,
+        null=True
+        )
+
+    ticket = models.CharField(
+        "Білет",
+        max_length=256,
+        blank=False
+        )
+
+    notes = models.TextField(
+        "Додаткові нотатки",
+        blank=True
+        )
