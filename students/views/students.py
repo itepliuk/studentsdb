618a33bd30510ae9a581538b2dad8c17ed5839a3
Igor Tepliuk <itepliuk@gmail.com>
2018-04-25 00:08:20 +0300

    Create add students template, edit student add view

--- students/views/students.py
+++ students/views/students.py
@@ -30,7 +30,7 @@ def students_list(request):
 	return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
-	return HttpResponse('<h1>Student Add Form</h1>')
+	return render(request, 'students/students_add.html', {})
 
 def students_edit(request, sid):
 	return HttpResponse('<h1>Edit Student %s</h1>' % sid)
