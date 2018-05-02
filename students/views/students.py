3fadde9f43f0de40cd64f4e26f56ed1883af9af9
Igor Tepliuk <itepliuk@gmail.com>
2018-05-03 01:31:57 +0300

    add new view for student add form

--- students/views/students.py
+++ students/views/students.py
@@ -1,39 +1,76 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from ..models import Student
+from ..models import Student, Group
 
 # Views for Students
 
 def students_list(request):
-	students = Student.objects.all()
-	
-	# try to order student list
-	order_by = request.GET.get('order_by', '')
-	if order_by in ('id','last_name', 'first_name', 'ticket'):
-		students = students.order_by(order_by)
-		if request.GET.get('reverse', '') == '1':
-			students = students.reverse()
+    students = Student.objects.all()
+    
+    # try to order student list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('id','last_name', 'first_name', 'ticket'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
 
-	# paginate students
-	paginator = Paginator(students, 3)
-	page = request.GET.get('page')
-	try:
-		students = paginator.page(page)
-	except PageNotAnInteger:
-		# If page is not an integer, deliver first page
-		students = paginator.page(1)
-	except EmptyPage:
-		#If page is out of range (e.g. 9999), deliver last page of results
-		students = paginator.page(paginator.num_pages)
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page
+        students = paginator.page(1)
+    except EmptyPage:
+        #If page is out of range (e.g. 9999), deliver last page of results
+        students = paginator.page(paginator.num_pages)
 
-	return render(request, 'students/students_list.html', {'students': students})
+    return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
-	return render(request, 'students/students_add.html', {})
+    # was form posted?
+    if request.method == "POST":
+        # was form add button clicked?
+        if request.POST.get('add_button') is not None:
+
+            # TODO: validate input from user
+            errors = {}
+
+            if not errors:
+                #create student object
+                student = Student(
+                    first_name=request.POST['first_name'],
+                    last_name=request.POST['last_name'],
+                    middle_name=request.POST['middle_name'],
+                    birthday=request.POST['birthday'],
+                    ticket=request.POST['ticket'],
+                    student_group=Group.objects.get(pk=request.POST['student_group']),
+                    photo=request.FILES['photo'],
+                    )
+                # save it to database
+                student.save()
+
+                # redirect user to students list
+                return HttpResponseRedirect(reverse('home'))
+
+            else:
+                # render form with errors and previous user input
+                return render(request, 'students/students_add.html',
+                    {'groups': Group.objects.all().order_by('title'),
+                    'errors': errors })
+        elif request.POST.get('cancel_button') is not None:
+            # redirect to homepage on cancel button
+            return HttpResponseRedirect(reverse('home'))
+    else:
+        #initial form render
+        return render(request, 'students/students_add.html',
+                    {'groups': Group.objects.all().order_by('title')})
 
 def students_edit(request, sid):
-	return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
 
 def students_delete(request, sid):
-	return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
