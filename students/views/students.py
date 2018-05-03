20002ddf5770abcd611df3d4e56ed5fdbc5e6e8c
Igor Tepliuk <itepliuk@gmail.com>
2018-05-04 01:02:01 +0300

    add validation to add students form

--- students/views/students.py
+++ students/views/students.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -36,20 +37,60 @@ def students_add(request):
         # was form add button clicked?
         if request.POST.get('add_button') is not None:
 
-            # TODO: validate input from user
+            # errors collection
             errors = {}
+            # validate student data will go here
+            data = {'middle_name': request.POST.get('middle_name'),
+            'notes': request.POST.get('notes')}
 
+            #validate user input
+            first_name = request.POST.get('first_name', '').strip()
+            if not first_name:
+                errors['first_name'] = "Ім'я є обов'язковим"
+            else:
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name', '').strip()
+            if not last_name:
+                errors['last_name'] = "Прізвище є обов'язковим"
+            else:
+                data['last_name'] = last_name
+                
+            birthday = request.POST.get('birthday', '').strip()
+            if not birthday:
+                errors['birthday'] = "Дата народження є обов'язковою"
+            else:
+                try:
+                    datetime.strptime(birthday, '%Y-%m-%d')
+                except Exception:
+                    errors['birthday'] = 'Введіть коректний формат дати (напр. 1991-12-25)'
+                else:
+                    data['birthday'] = birthday
+
+            ticket = request.POST.get('ticket', '').strip()
+            if not ticket:
+                errors['ticket'] = "Номер білета є обов'язковим"
+            else:
+                data['ticket'] = ticket
+
+            student_group = request.POST.get('student_group', '').strip()
+            if not student_group:
+                errors['student_group'] = "Оберіть групу для студента"
+            else:
+                groups = Group.objects.filter(pk=student_group)
+                if len(groups) != 1:
+                    errors['student_group'] = 'Оберіть коректну групу'
+                else:
+                    data['student_group'] = groups[0]
+
+            photo = request.FILES.get('photo')
+            if photo:
+                data['photo'] = photo
+
+            # save student
             if not errors:
-                #create student object
-                student = Student(
-                    first_name=request.POST['first_name'],
-                    last_name=request.POST['last_name'],
-                    middle_name=request.POST['middle_name'],
-                    birthday=request.POST['birthday'],
-                    ticket=request.POST['ticket'],
-                    student_group=Group.objects.get(pk=request.POST['student_group']),
-                    photo=request.FILES['photo'],
-                    )
+                #create student object from data {}
+                student = Student(**data)
                 # save it to database
                 student.save()
 
