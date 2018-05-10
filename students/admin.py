66d85c8a12a06c1cf97227aa37628df884d449c5
Igor Tepliuk <itepliuk@gmail.com>
2018-05-10 21:58:55 +0300

    extend admin student and group

--- students/admin.py
+++ students/admin.py
@@ -1,11 +1,42 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 from .models import Student, Group, Exam, Rating
 
+
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+	list_display_links = ['last_name', 'first_name']
+	list_editable = ['student_group']
+	ordering = ['last_name']
+	list_filter = ['student_group']
+	list_per_page =  10
+	search_fields = ['last_name', 'first_name', 'middle_name', 'ticket', 'notes']
+
+	def view_on_site(self, obj):
+		return reverse('students_edit', kwargs={'pk': obj.id})
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+	list_display = ['title', 'leader', 'notes']
+	list_display_links = ['title']
+	list_editable = ['leader']
+	ordering = ['title']
+	list_filter = ['leader']
+	list_per_page =  10
+	search_fields = ['title', 'leader', 'notes']
+
+	def view_on_site(self, obj):
+		return reverse('groups_edit', kwargs={'pk': obj.id})
+
+
+
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Group)
+admin.site.register(Student, StudentAdmin)
+
 admin.site.register(Exam)
 admin.site.register(Rating)
-# @admin.register(Student)
+
 # class StudentAdmin(admin.ModelAdmin):
 # 	pass
+
