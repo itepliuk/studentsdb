0ef2783e6037bb437614803c12c897e3937aa793
Igor Tepliuk <itepliuk@gmail.com>
2019-01-26 00:23:30 +0200

    AJAX for journal on checkboxes, post method to journal view

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -1,8 +1,34 @@
 function initJournal() {
-    $('.day-box input[type="checkbox"]').click(function(event){
-        alert('test click js');
+    var indicator = $('#ajax-progress-indicator');
+
+    $('.day-box input[type="checkbox"]').on('click', function(event){
+        var box = $(this);
+        $.ajax(box.data('url'), {
+            'type': 'POST',
+            'async': true,
+            'dataType': 'json',
+            'data': {
+            'pk': box.data('student-id'),
+            'date': box.data('date'),
+            'present': box.is(':checked') ? '1': '',
+            'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
+            },
+            'beforeSend': function(xhr, settings){
+                indicator.show();
+            },
+            'error': function(xhr, status, error){
+                alert(error);
+                indicator.hide();
+            },
+            'success': function(data, status, xhr){
+                indicator.hide();
+            }
+        });
     });
 }
+        
 $(document).ready(function(){
-    initJournal();
+     initJournal();
+
 });
+
--- students/views/journal.py
+++ students/views/journal.py
@@ -2,6 +2,7 @@ from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 from calendar import monthrange, weekday, day_abbr
 
+from django.http import JsonResponse
 from django.views.generic.base import reverse
 from django.views.generic.base import TemplateView
 
@@ -102,6 +103,22 @@ class JournalView(TemplateView):
         # finally return updated context
         # with paginated students
         return context
+    
+    def post(self, request, *args, **kwargs):
+        data = request.POST
 
+        # prepare student, dates and presence data
+        current_date = datetime.strptime(data['date'], '%Y-%m-%d').date()
+        month = date(current_date.year, current_date.month, 1)
+        present = data['present'] and True or False
+        student = Student.objects.get(pk=data['pk'])
 
+        # get or create journal object for given student and month
+        journal = MonthJournal.objects.get_or_create(student=student, date=month)[0]
 
+        # get new presence on journal for given student and save result
+        setattr(journal, 'present_day%d' % current_date.day, present)
+        journal.save()
+
+        # return success status
+        return JsonResponse({'status': 'success'})
